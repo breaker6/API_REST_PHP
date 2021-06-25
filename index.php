66f31b6d9a1be2aa6b0535ea87819a7fdb11f246
breaker6 <62377366+breaker6@users.noreply.github.com>
2021-06-25 12:45:04 +0200

    Inicio de API en PHP desde cero sin frameworks

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//Añadimos la clase conexion.php
+require_once "clases/conexion/conexion.php";
+
+//Declaramos la variable conexión de la clase para comprobar que funciona
+$conexion = new conexion;
+
+
+?>
