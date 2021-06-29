d4951aaaeeff85b98038fecb34e06af05329a732
breaker6 <62377366+breaker6@users.noreply.github.com>
2021-06-29 15:10:41 +0200

    Actualizado Autentificacion

--- auth.php
+++ auth.php
@@ -11,11 +11,31 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 	//Recogemos los datos que recibimos
     $postBody = file_get_contents("php://input");
+    //Enviamos los datos al manejador
     $datosArray = $_auth->login($postBody);
-    print_r(json_encode($datosArray));
+    //Devolvemos una respuesta. Le decimos primero el tipo de respuesta que será (un json)
+    header('Content-Type: application/json');
+    //Comprobamos si hay un error
+    if(isset($datosArray["result"]["error_id"])){
+    	//Si lo hay, devolvemos la respuesta
+        $responseCode = $datosArray["result"]["error_id"];
+        http_response_code($responseCode);
+    }else{
+    	//Si no hay error, devolveremos el codigo 200
+        http_response_code(200);
+    }
+    //Imprimimos el json
+    echo json_encode($datosArray);
 
 //Si no, da error
 }else{
+	//Le decimos que la respuesta será un json
+    header('Content-Type: application/json');
+    //Escribiremos en $datosArray el error
+    $datosArray = $_respuestas->error_405();
+    //Lo imprimimos
+    echo json_encode($datosArray);
+
     echo " metodo no permitido";
 }
 
